ec1d03505689b17117a7b8ea6f57a861aea38244
Laura Read <laurajmtb@gmail.com>
2024-09-11 11:36:40 +0000

    Add update field to post model

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+# Create the post (Blog Post) models here.
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
@@ -12,6 +12,15 @@ class Post(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
-    comment = models.TextField()
+    content = models.TextField()
+    excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    # Foreign key to link blog posts directly to services offered
+    # Commented out until service model is added
+    # service = models.ForeignKey(
+    # Service, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
