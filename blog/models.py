7e1d5652e8d2e1d61ccf72eb2a1d9efe3292ff5c
Laura Read <laurajmtb@gmail.com>
2024-09-11 18:50:15 +0000

    If user profile is dleted, comments remain.  Create dynamic Blog urls. Create base HTML for home page.

--- blog/models.py
+++ blog/models.py
@@ -17,24 +17,39 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    # SEO Tags for the blog post (Commented out until the tags model is made)
+    # seo_tags = models.TextField(
+    # blank=True, help_text="Add SEO keywords, separated by commas")
     # Foreign key to link blog posts directly to services offered
-    # Commented out until service model is added
+    # (Commented out until service model is added)
     # service = models.ForeignKey(
     # Service, on_delete=models.SET_NULL, null=True, blank=True)
 
+    class Meta:
+        ordering = ["-created_on"]
+
     def __str__(self):
-        return self.title
+        return f"{self.title} | written by {self.author}"
 
 
 class Comment(models.Model):
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name="comments"
     )
-    # I don't think I want the comments to delete if user deletes themselves?
+    # If the commenter deletes their profile, their comments will remain but
+    # author field will be null
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="Commenter"
+        User, on_delete=models.SET_NULL, null=True, related_name="comments_author"
     )
     # What should comments max length be?
     body = models.TextField(max_length=500)
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    # Display firt 50 characters in the admin panel. Check if this string
+    # supplies the comment in other views
+    def __str__(self):
+        return f"{self.author} | {self.body[:50]}..."
