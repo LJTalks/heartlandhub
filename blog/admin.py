a1e755e193d2c353120e79f0cc540c1dacc62504
Laura Read <laurajmtb@gmail.com>
2024-09-12 11:30:54 +0000

    Add search and filters t admin panel. Add bog posts frm json file

--- blog/admin.py
+++ blog/admin.py
@@ -6,11 +6,19 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
-    list_display = ('title', 'slug', 'status')
-    search_fields = ['title']
-    list_filter = ('status',)
+    # Added seo_list status to see if it has data
+    list_display = ('title', 'slug', 'status', 'created_on', 'seo_tags_status')
+    search_fields = ['title', 'content', 'seo_tags']
+    list_filter = ('status', 'created_on',)
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
+
+# Custom method to display if seo_tags is empty or not
+    def seo_tags_status(self, obj):
+        return "Yes" if obj.seo_tags else "No"
+
+    # Adding a short description for the list view column
+    seo_tags_status.short_description = 'SEO Tags'
 
 
 # Register your models here.
