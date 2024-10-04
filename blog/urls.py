f73eb61cc1b028acc30103db35526a70ac137e07
Laura Read <laurajmtb@gmail.com>
2024-10-04 19:27:40 +0000

    Add Cloudinary for image handling

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,23 @@
-from django.urls import path
 from . import views
+from django.urls import path
+
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),  # List of blog posts
-    path('<slug:slug>/', views.post_detail, name='post_detail'),  # Single post detail
+    # Single post detail
+    path('<slug:slug>/',
+         views.post_detail, name='post_detail'),
+]
+
+
+from . import views
+from django.urls import path
+
+
+urlpatterns = [
+    # this is the blog home page that has all the blogs
+    # The blog home page/list view
+    path("", views.PostList.as_view(), name="home"),
+    # Individual Blog posts with slug address
+    path('<slug:slug>/', views.post_detail, name='post_detail'), 
 ]
--- ljtalks/settings.py
+++ ljtalks/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_summernote',
+    'cloudinary_storage',
+    'cloudinary',
     'blog',
     # 'services',
     # 'booking',
@@ -138,6 +140,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
