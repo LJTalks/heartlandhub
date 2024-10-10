53654c1d2ecae6d2a037972f4c79344d13e23051
Laura Read <laurajmtb@gmail.com>
2024-10-10 15:21:21 +0000

    Deleted previous commented out static file setting for heroku test

--- ljtalks/settings.py
+++ ljtalks/settings.py
@@ -147,7 +147,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # for collectstatic
 
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
