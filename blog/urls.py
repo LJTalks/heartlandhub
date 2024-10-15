e64eb73b7735e8883b39fe1609f3e000817dea1c
Laura Read <laurajmtb@gmail.com>
2024-10-15 11:14:45 +0000

    Debug access to about_me page

--- blog/urls.py
+++ blog/urls.py
@@ -4,19 +4,13 @@ from . import views
 
 urlpatterns = [
     # My About Me page URL
-    path('about/', views.about_me, name='about_me'),
+    path('about_me/', views.about_me, name='about_me'),
     # this is the blog home page that has all the blogs
     # The blog home page/list view
     # Not sure where this came from
     # It's a view in blogs, move to Profile when it exists
     path('unsubscribe/<int:user_id>/', views.unsubscribe, name='unsubscribe'),
     # Individual Blog posts with slug address
-    path('<slug:slug>/', views.post_detail, name='post_detail'),
-    path("", views.PostList.as_view(), name="home"),
-]
-
-
-urlpatterns = [
     path("", views.PostList.as_view(), name="home"),
     path("<slug:slug>/", views.post_detail, name="post_detail"),
     path(
