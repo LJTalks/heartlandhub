36632b310f4d1a199137ebe6ab111654074545ad
Laura Read <laurajmtb@gmail.com>
2024-10-21 18:42:11 +0000

    Add honeytrap to signupform and  logging for bot detection

--- ljtalks/forms.py
+++ ljtalks/forms.py
@@ -1,7 +1,12 @@
 from allauth.account.forms import SignupForm
 from django import forms
 from ljtalks.models import UserProfile
+import logging
 # from captcha.fields import ReCaptchaField
+
+
+# Set up the logger for bot detection
+logger = logging.getLogger(__name__)
 
 
 class ContactForm(forms.Form):
@@ -13,12 +18,22 @@ class ContactForm(forms.Form):
 
 
 class CustomSignupForm(SignupForm):
+    honeypot = forms.CharField(required=False, widget=forms.HiddenInput)
+    
     def save(self, request):
+        if self.cleaned_data['honeypot']:
+            logger.warning(
+                f"Bot detected during signup! IP: {request.META.get(
+                    'REMOTE_ADDR')}")
+            
+            raise forms.ValidationError("Bot detected!")
+        
         user = super(CustomSignupForm, self).save(request)
         
         # Create a UserProfile and save the source and IP
         source = request.META.get('HTTP_REFERER', '')
-        ip_address = request.META.get('REMOTE_ADDR', '')
+        ip_address = request.META.get(
+            'HTTP_X_FORWARDED_FOR', request.META.get('REMOTE_ADDR', ''))
         
         # After saving the user, update the profile or create a profile with
         # the tracking info
